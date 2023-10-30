1b6ec82460c438fcb48651a9284aeb4de3704bae
Ji-Hoon24 <oh.ji.hoon24@gmail.com>
2023-10-30 14:22:11 +0900

    JUnit 맛보기

--- test/jhtest/ScoreCollectionTest.java
+++ test/jhtest/ScoreCollectionTest.java
@@ -1,13 +1,28 @@
 package jhtest;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.*;
 
 public class ScoreCollectionTest {
 
    @Test
+   public void answersArithmeticMeanOfTwoNumbers() {
+      // 준비
+      ScoreCollection collection = new ScoreCollection();
+      collection.add(() -> 5);
+      collection.add(() -> 7);
+
+      // 실행
+      int actualResult = collection.arithmeticMean();
+
+      // 단언
+      assertThat(actualResult, equalTo(6));
+   }
+
+   @Test
    public void test() {
-      fail("Not yet implemented");
+//      fail("Not yet implemented");
    }
 
 }
